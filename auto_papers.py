9287a7e68fd76677551ea6ca729e084f134d1394
Anthony Morada <anthony.omorada@gmail.com>
2020-05-16 08:16:38 -0700

    File moving with pdf selection

---
+++ auto_papers.py
@@ -0,0 +1,34 @@
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+from datetime import date
+
+import os
+import json
+import time
+import shutil
+
+today = date.today()
+current_date = today.strftime("%Y/%m/%d")
+
+class MyHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        for filename in os.listdir(folder_to_track):
+            if filename.endswith('.pdf'):
+                src = folder_to_track + "/" + filename
+                new_destination = folder_destination + "/" + filename
+                os.rename(src, new_destination)
+
+            
+folder_to_track = "/Users/anthonymorada/Desktop/myFolder"
+folder_destination = "/Users/anthonymorada/Desktop/newFolder"
+event_handler = MyHandler()
+observer = Observer()
+observer.schedule(event_handler, folder_to_track, recursive = True)
+observer.start()
+
+try:
+    while True:
+        time.sleep(10)
+except KeyboardInterrupt:
+    observer.stop()
+observer.join
